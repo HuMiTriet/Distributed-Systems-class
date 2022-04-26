552803734fb63eae5cffcf6c9d40be967daeddfd
Triet Huynh <triet403@gmail.com>
2022-04-26 21:23:01 +0700

    Revert "main"
    This reverts commit 1c3d5ad951b4793a3c9d7ba9984860532605181f.

--- src/main/java/com/personal/App.java
+++ src/main/java/com/personal/App.java
@@ -2,8 +2,11 @@ package com.personal;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("MAIN");
-    System.out.println("MAIN");
-    System.out.println("MAIN");
+    System.out.println("bruh 1");
+
+    System.out.println("bruh 2");
+
+    System.out.println("bruh 4");
+    System.out.println("bruh 5");
   }
 }
