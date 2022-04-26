1c3d5ad951b4793a3c9d7ba9984860532605181f
Triet Huynh <triet403@gmail.com>
2022-04-26 21:15:04 +0700

    main
    main 2
    
    main 2

--- src/main/java/com/personal/App.java
+++ src/main/java/com/personal/App.java
@@ -2,11 +2,8 @@ package com.personal;
 
 public class App {
   public static void main(String[] args) {
-    System.out.println("bruh 1");
-
-    System.out.println("bruh 2");
-
-    System.out.println("bruh 4");
-    System.out.println("bruh 5");
+    System.out.println("MAIN");
+    System.out.println("MAIN");
+    System.out.println("MAIN");
   }
 }
