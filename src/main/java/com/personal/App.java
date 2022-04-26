b8efba84100b5f0b00bd44745dced9742b29cb68
Triet Huynh <triet403@gmail.com>
2022-04-26 21:38:09 +0700

    more test

--- src/main/java/com/personal/App.java
+++ src/main/java/com/personal/App.java
@@ -3,5 +3,9 @@ package com.personal;
 public class App {
   public static void main(String[] args) {
     System.out.println("TEST");
+    System.out.println("TEST");
+    System.out.println("TEST");
+    System.out.println("TEST");
+    System.out.println("TEST");
   }
 }
